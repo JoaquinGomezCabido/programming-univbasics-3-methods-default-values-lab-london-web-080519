6bf6488a9758fad4132fb2f57107000a181a145d
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-12 19:03:59 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,9 @@
 # Your code here
 
 def meal_choice(vegetables, carbs, protein="meat")
-  puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{vegetables} and #{carbs}."
+  if(protein == "meat")
+    puts "A plate of #{protein} with #{vegetables} and #{carbs}."
+  else
+    puts "What a nutritious meal!"
+    puts "A plate of #{protein} with #{vegetables} and #{carbs}."
 end
